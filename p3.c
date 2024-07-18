b430dbb37d18c06ed405596c94fc96dff51f1a53
imyou69 <121504604+imyou69@users.noreply.github.com>
2024-07-18 20:43:10 +0530

    Update p3.c

--- p3.c
+++ p3.c
@@ -1,23 +1,41 @@
-#include <stdio.h>
-#include<unistd.h>
-int main(int argc, char *argv[])
-{
-if(argc!=3)
-{
-printf(“usage: %s <src_file><dest_file>\n”,argv[0]);
-Return 0;
-}
-if(link(argv[1],argv[2])==-1)
-{
-printf(“link error\n”);
-return 1;
-}
-printf(“files linked\n”);
-printf(“Inode number of linked files\n”);
+#include<stdio.h>
+void swap(int *a, int *b){
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+	}
 
-char str[100];
-sprintf(str,”ls –i %s %s \n”,argv[1],argv[2]);
-system(str);
+int partition(int A[], int low, int high){
+int pivot  = A[low];
+int j = high;
+for(int i = high; i>low;i--){
+	if(A[i]>pivot)
+		swap(&A[i],&A[j--]);
+}
+swap(&A[low],&A[j]);
+return j;
+}
+
+void Quick(int A[], int start, int end){
+if(start<end){
+int index = partition(A,start,end);
+Quick(A,start,index-1);
+Quick(A,index+1,end);
+}
+}
+
+void show(int A[], int size){
+for(int i = 0; i< size;i++){
+	printf("%d ",A[i]);
+		}
+printf("\n");
+}
+
+int main(){
+int A[] = { 8,1,0,12,9,2,4,16,7,21};
+int size = sizeof(A)/sizeof(A[0]);
+Quick(A,0,size-1);
+printf("Sorted Array : \n");
+show(A,size);
 return 0;
 }
-
