5cc50396bcfd2f1b8685f95225028d16065629b1
imyou69 <121504604+imyou69@users.noreply.github.com>
2024-07-18 20:42:45 +0530

    Update p2.c

--- p2.c
+++ p2.c
@@ -1,57 +1,34 @@
-#include <stdio.h>
-#include <stdlib.h
-#include <fcntl.h>
-#include <errno.h>
- 
-#define BUFF_SIZE 1024
- 
-int main(int argc, char* argv[])
-{
-	int srcFD,destFD,nbread,nbwrite;
-	char *buff[BUFF_SIZE];
-	
-	/*Check if both src & dest files are received*/
-	if(argc != 3)
-	{
-		printf("\nUsage: cpcmd source_file destination_file\n");
-		exit(EXIT_FAILURE);
+#include<stdio.h>
+#include<time.h>
+
+void print(int A[], int n){
+for(int i = 0; i< n; i++){
+	printf("%d \t",A[i]);
 	}
- 
-	/*Open source file*/
-	srcFD = open(argv[1],O_RDONLY);
- 
-	if(srcFD == -1)
-	{
-		printf("\nError opening file %s errno = %d\n",argv[1],errno);
-		exit(EXIT_FAILURE);	
-	}
-	
-	/*Open destination file with respective flags & modes
-	  O_CREAT & O_TRUNC is to truncate existing file or create a new file
-	  S_IXXXX are file permissions for the user,groups & others*/
-	destFD = open(argv[2],O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR |    S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
- 
-	if(destFD == -1)
-	{
-		printf("\nError opening file %s errno = %d\n",argv[2],errno);
-		exit(EXIT_FAILURE);
-	}
- 
-	/*Start data transfer from src file to dest file till it reaches EOF*/
-	while((nbread = read(srcFD,buff,BUFF_SIZE)) > 0)
-	{
-		if(write(destFD,buff,nbread) != nbread)
-			printf("\nError in writing data to %s\n",argv[2]);
-	}
-	
-	if(nbread == -1)
-		printf("\nError in reading data from %s\n",argv[1]);
-	
-	if(close(srcFD) == -1)
-		printf("\nError in closing file %s\n",argv[1]);
- 
-	if(close(destFD) == -1)
-		printf("\nError in closing file %s\n",argv[2]);
- 
-	exit(EXIT_SUCCESS);
+	printf("\n");
+}
+
+void inssort(int A[], int n){
+for(int i = 1; i< n; i++){
+    int key = A[i];
+    int j = i - 1;
+    while (key < A[j] && j >= 0) {
+      A[j + 1] = A[j];
+      --j;
+    }
+    A[j + 1] = key;
+  }
+}
+
+int main(){
+clock_t end, start;
+int A[] = { 3,15,14,2,7,9,21};
+int size = sizeof(A)/sizeof(A[0]);
+start = clock();
+inssort(A,size);
+printf("Array in ascending order : \n");
+print(A,size);
+end = clock();
+printf("Time taken to perform insertion sort %f",(double)(end-start)/CLOCKS_PER_SEC);
+return 0;
 }
